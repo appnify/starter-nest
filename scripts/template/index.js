ad27bab0c558a8b14f1b4683a4f7d102b746f567
luoer <952222@163.com>
2023-08-04 09:53:17 +0800

    feat: 优化生成模板

--- scripts/template/index.js
+++ scripts/template/index.js
@@ -10,12 +10,23 @@ module.exports = function main(/** @type { import('plop').NodePlopAPI } */ plop)
     return filename.charAt(0).toUpperCase() + filename.slice(1);
   });
 
+  plop.setHelper('lowcaseName', (name) => {
+    const filename = plop.getHelper('fileName')(name);
+    return filename.charAt(0).toLowerCase() + filename.slice(1);
+  });
+
   plop.setGenerator('module', {
     prompts: [
       {
         name: 'name',
         message: '请输入模块名称',
         type: 'input',
+        validate: (input) => {
+          if (/^(\w+\/)*(\w+)$/.test(input)) {
+            return true;
+          }
+          return '请输入 <moduleName> 或 <dirName>/<moduleName> 格式, 且不包含中文的名称';
+        },
       },
       {
         name: 'cnName',
@@ -28,7 +39,7 @@ module.exports = function main(/** @type { import('plop').NodePlopAPI } */ plop)
         type: 'addMany',
         destination: join(process.cwd(), 'src/{{name}}'),
         base: 'module',
-        templateFiles: 'module/**/*.ts',
+        templateFiles: 'module/**/*.hbs',
       },
     ],
   });
